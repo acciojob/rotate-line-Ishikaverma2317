240f4ffa55271b34c669376b9cf7bf75d8979f11
Ishika Verma <ishikaverma2317@gmail.com>
2025-11-11 09:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let angle = 0; // initial rotation angle
+const line = document.getElementById("line");
+
+// function to update rotation
+setInterval(() => {
+  angle += 2; // increase by 2 degrees
+  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+}, 20); // update every 20 milliseconds
